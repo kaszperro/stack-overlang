7a327c58e019bee31aba1220f660c028b7164ee1
Kasper Sapała <kasper.sapala@gmail.com>
2019-05-16 11:45:15 +0200

    Snippet update

--- src/main/scala/OverlangMain.scala
+++ src/main/scala/OverlangMain.scala
@@ -2,7 +2,7 @@ object OverlangMain {
   def main(args: Array[String]): Unit = {
 
     val snippet = StackOverflowSnippet(21991967)
-    
+
     println(snippet.getCode)
     /*
       Code(
--- src/main/scala/StackOverflowSnippet.scala
+++ src/main/scala/StackOverflowSnippet.scala
@@ -6,63 +6,45 @@ final class WrongBlockNumberException(private val message: String = "",
   extends Exception(message, cause)
 
 
-class StackOverflowSnippet(private var codeBlock: Either[String, List[String]]) extends Snippet {
-
+class StackOverflowSnippet(private var codeBlocks: List[String]) extends Snippet {
 
   override def getCode: String = {
-
-    codeBlock match {
-      case Left(str) => str
-      case Right(list) => list.foldLeft("") { (acc, str) => acc + "\n" + str }
-    }
-
+    codeBlocks.foldLeft("") { (acc, str) => acc + "\n" + str }
   }
 
-  def block(blocksNumber: Range): StackOverflowSnippet = {
-    codeBlock match {
-      case Left(_) => throw new RuntimeException("cat get range from string")
-      case Right(list) => new StackOverflowSnippet(Right(
-        for {i <- blocksNumber.toList} yield list(i)
-      ))
-    }
-  }
+  def block(blocksNumber: Range): StackOverflowSnippet =
+    new StackOverflowSnippet(
+      for {i <- blocksNumber.toList} yield codeBlocks(i)
+    )
+
 
   def block(blockIndex: Int): StackOverflowSnippet = {
-    codeBlock match {
-      case Left(_) => throw new RuntimeException("cant get block of code")
-      case Right(list) => new StackOverflowSnippet(Left(list(blockIndex)))
-    }
+    new StackOverflowSnippet(List(codeBlocks(blockIndex)))
   }
+  
 }
 
 object StackOverflowSnippet {
   def apply(id: Int): StackOverflowSnippet = {
-    new StackOverflowSnippet(Right(
+    new StackOverflowSnippet(
       parseResponseToListOfCode(getAnswerAsString(id))
-    ))
+    )
   }
 
   def apply(id: Int, blockNumber: Int): StackOverflowSnippet = {
     val codeList = parseResponseToListOfCode(getAnswerAsString(id))
-    new StackOverflowSnippet(Left(codeList(blockNumber)))
+    new StackOverflowSnippet(List(codeList(blockNumber)))
   }
 
   def apply(id: Int, blocksNumbers: Range): StackOverflowSnippet = {
     val codeList = parseResponseToListOfCode(getAnswerAsString(id))
-    val newCodeList = for {i <- blocksNumbers.toList} yield codeList(i)
-    new StackOverflowSnippet(Right(newCodeList))
+    new StackOverflowSnippet(for {i <- blocksNumbers.toList} yield codeList(i))
   }
 
 
-  /*
+  def apply(stackOverflowAnswer: StackOverflowAnswer): StackOverflowSnippet =
+    new StackOverflowSnippet(stackOverflowAnswer.codeBlocks)
 
-    def apply(stackOverflowAnswer: StackOverflowAnswer, blockNumber: Int): StackOverflowSnippet = {
-      val mySnippet = new StackOverflowSnippet(stackOverflowAnswer.id, blockNumber)
-      mySnippet.codeBlocksList = Some(stackOverflowAnswer.codeBlocks)
-      mySnippet
-    }
-
-  */
 }
 
 
