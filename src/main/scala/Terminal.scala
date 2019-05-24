9729abb35780c48ee00605a735e161e4996688f0
jakubdybczak <jakub.dybczak@gmail.com>
2019-05-25 00:38:54 +0200

    Add new Labels widget

--- src/main/scala/Terminal.scala
+++ src/main/scala/Terminal.scala
@@ -1,5 +1,7 @@
 import net.team2xh.scurses.{Colors, Scurses}
-import terminal.{Frame, Input, TestWidget}
+import terminal.{Frame, Input, Labels, TestWidget}
+
+import scala.collection.mutable.ListBuffer
 
 
 object Terminal extends App {
@@ -7,18 +9,39 @@ object Terminal extends App {
     val frame = Frame(Some("Stack-Overlang"))
 
 
-    new TestWidget(frame.panel, Colors.DIM_RED,
-      () => frame.panel.innerWidth / 2, () => frame.panel.innerHeight / 2,
-      () => 0, () => 0)
+    //    new TestWidget(frame.panel, Colors.DIM_RED,
+    //      () => frame.panel.innerWidth / 2, () => frame.panel.innerHeight / 2,
+    //      () => 0, () => 0)
+    //
+    //    new TestWidget(frame.panel, Colors.DIM_BLUE,
+    //      () => frame.panel.innerWidth / 2, () => frame.panel.innerHeight / 2,
+    //      () => frame.panel.innerWidth / 4, () => frame.panel.innerHeight / 4)
+    //
+    //    Input(frame.panel, "text",
+    //      widthFun = () => frame.panel.innerWidth, heightFun = () => 1,
+    //      offsetYFun = () => frame.panel.innerHeight - 1)
 
-    new TestWidget(frame.panel, Colors.DIM_BLUE,
-      () => frame.panel.innerWidth / 2, () => frame.panel.innerHeight / 2,
-      () => frame.panel.innerWidth / 4, () => frame.panel.innerHeight / 4)
+    var testString = "Siemanko testowy tekst"
+    var testString2 = "Siemanko testowy tekst długi bardzo ijdsiadjaoisdjoisajdoiasjdoiajsoidaisodhasoiudhoiashdioashdoiasi"
+
+    var listBuff = new ListBuffer[String]
+    for (i <- 1 to 20) {
+      listBuff += i + ". " + testString
+      listBuff += i + ". " + testString2
+    }
+
+    val list = listBuff.toList
+
+    new Labels[String](frame.panel, list, s => s, s => {},
+      () => frame.panel.innerWidth, () => frame.panel.innerHeight - 1,
+      () => 0, () => 0)
 
     Input(frame.panel, "text",
       widthFun = () => frame.panel.innerWidth, heightFun = () => 1,
       offsetYFun = () => frame.panel.innerHeight - 1)
 
+
     frame.show()
+
   }
 }
---
+++ src/main/scala/terminal/Labels.scala
@@ -0,0 +1,103 @@
+package terminal
+
+
+import net.team2xh.onions.Component
+import net.team2xh.onions.Themes.ColorScheme
+import net.team2xh.onions.components.{FramePanel, Widget}
+import net.team2xh.onions.utils.{Varying, TextWrap}
+import net.team2xh.scurses.{Keys, Colors, Scurses}
+
+case class Labels[A](parent: FramePanel, elements: List[A], textExtractor: A => String, onAction: A => Unit,
+                     widthFun: () => Int, heightFun: () => Int,
+                     offsetXFun: () => Int, offsetYFun: () => Int)
+                    (implicit screen: Scurses) extends Widget(parent) {
+
+  private var actualFocusedElement = 0
+  private var firstElementIndex = 0
+  private var lastElementIndex = -1
+  private var firstLine = 0
+
+
+  override def redraw(focus: Boolean, theme: ColorScheme): Unit = {
+    var wrappedLines = 0
+    for (i <- elements.indices) {
+      val lines = TextWrap.wrapText(textExtractor(elements(i)), innerWidth - 1, TextWrap.ALIGN_LEFT)
+      for (line <- lines) {
+        if (wrappedLines == firstLine) firstElementIndex = i
+        val draw = wrappedLines >= firstLine
+        if (draw) {
+          val color = if (i == actualFocusedElement) Colors.BRIGHT_GREEN else Colors.BRIGHT_RED
+          val color2 = if (focus) Colors.DIM_BLUE else Colors.DIM_MAGENTA
+          screen.put(0, wrappedLines - firstLine, " " + line + " " * (innerWidth - line.length - 1), color, color2)
+        }
+        wrappedLines = wrappedLines + 1
+        lastElementIndex = i
+        if (wrappedLines - firstLine >= innerHeight)
+          return
+      }
+    }
+  }
+
+  private def getFirstLineOfElement(index: Int): Int = {
+    var wrappedLines = 0
+    for (i <- elements.indices) {
+      val lines = TextWrap.wrapText(textExtractor(elements(i)), innerWidth - 1, TextWrap.ALIGN_LEFT)
+      if (i == index) return wrappedLines
+      for (_ <- lines) {
+        wrappedLines = wrappedLines + 1
+      }
+    }
+    -1
+  }
+
+  private def getLinesSize(index: Int): Int = {
+    var wrappedLines = 0
+    val lines = TextWrap.wrapText(textExtractor(elements(index)), innerWidth - 1, TextWrap.ALIGN_LEFT)
+    for (_ <- lines) {
+      wrappedLines = wrappedLines + 1
+    }
+    wrappedLines
+  }
+
+  override def handleArrowPress(keypress: Int): Boolean = {
+    var handled = false
+    if (keypress == Keys.UP) {
+      if (actualFocusedElement > 0) {
+        actualFocusedElement = actualFocusedElement - 1
+        handled = true
+      }
+      if (actualFocusedElement <= firstElementIndex) {
+        firstLine = getFirstLineOfElement(actualFocusedElement)
+      }
+    }
+    if (keypress == Keys.DOWN) {
+      if (actualFocusedElement < elements.size - 1) {
+        actualFocusedElement = actualFocusedElement + 1
+        handled = true
+      }
+      if (actualFocusedElement >= lastElementIndex) {
+        firstLine = getFirstLineOfElement(actualFocusedElement) - (innerHeight - getLinesSize(actualFocusedElement))
+        if (firstLine < 0) firstLine = 0;
+      }
+    }
+    if (keypress == Keys.UP || keypress == Keys.DOWN)
+      needsRedraw = true
+
+    handled
+  }
+
+
+  override def handleKeypress(keypress: Int): Unit = {
+
+  }
+
+  override def focusable = true
+
+  override def offsetX: Int = this.offsetXFun()
+
+  override def offsetY: Int = this.offsetYFun()
+
+  override def innerWidth: Int = widthFun()
+
+  override def innerHeight: Int = heightFun()
+}
