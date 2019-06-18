fb114e085fef5dc508ab787d6670fe70ec496615
jakubdybczak <jakub.dybczak@gmail.com>
2019-06-18 22:32:26 +0200

    Change frame title, Add args support

--- src/main/scala/overlang/Terminal.scala
+++ src/main/scala/overlang/Terminal.scala
@@ -5,45 +5,58 @@ import java.io.File
 import net.team2xh.scurses.Scurses
 import overlang.terminal.{ClickableLabels, ExternalEditor, Frame, FrameStack, Input, Labels, SearchResultsFrame}
 
-object Terminal extends App {
-  def saveAs(str: String): Unit = {
+object Terminal {
+  def saveAs(frame: Frame, str: String): Unit = {
     val text: String = ActiveFile.readAll
     ActiveFile.setFile(new File(str))
     ActiveFile.save(text)
+    updateTitle(frame)
   }
 
-  ActiveFile.setFile(File.createTempFile("overlang", "tmp"))
-
-
-  Scurses { implicit screen =>
-    val stack = new FrameStack
-    val frame = Frame(Some("Stack-Overlang"))
-
-
-    Labels(frame.panel, Help.text,
-      () => frame.panel.innerWidth, () => frame.panel.innerHeight - 1,
-      () => 0, () => 0)
-
-    Input(frame.panel, "text",
-      (text) => {
-        val addPattern = """add (\d+)""".r
-        val searchPattern = """search (.+)""".r
-        val saveAsPattern = """saveas (.+)""".r
-        val editPattern = """edit""".r
-        text match {
-          case addPattern(_) =>
-          case saveAsPattern(filePath) => saveAs(filePath)
-          case searchPattern(text) => stack.add(SearchResultsFrame(text))
-          case editPattern() => ExternalEditor.editFile(frame, ActiveFile.getFile)
-          case _ =>
-        }
-        Unit
-      },
-      widthFun = () => frame.panel.innerWidth, heightFun = () => 1,
-      offsetYFun = () => frame.panel.innerHeight - 1)
-
-    stack.add(frame)
-    stack.show()
+  def updateTitle(frame: Frame): Unit = {
+    frame.setTitle(Some(ActiveFile.getFile.getName + " - Stack-Overlang"))
 
   }
+
+  def main(args: Array[String]): Unit = {
+    ActiveFile.setFile(File.createTempFile("overlang", "tmp"))
+
+    Scurses { implicit screen =>
+      val stack = new FrameStack
+      val frame = Frame(Some("temporary file - Stack-Overlang"))
+
+      if (args.length > 0) {
+        ActiveFile.setFile(new File(args(0)))
+        updateTitle(frame)
+      }
+
+      Labels(frame.panel, Help.text,
+        () => frame.panel.innerWidth, () => frame.panel.innerHeight - 1,
+        () => 0, () => 0)
+
+      Input(frame.panel, "text",
+        (text) => {
+          val addPattern = """add (\d+)""".r
+          val searchPattern = """search (.+)""".r
+          val saveAsPattern = """saveas (.+)""".r
+          val editPattern = """edit""".r
+          text match {
+            case addPattern(_) =>
+            case saveAsPattern(filePath) => saveAs(frame, filePath)
+            case searchPattern(text) => stack.add(SearchResultsFrame(text))
+            case editPattern() => ExternalEditor.editFile(frame, ActiveFile.getFile)
+            case _ =>
+          }
+          Unit
+        },
+        widthFun = () => frame.panel.innerWidth, heightFun = () => 1,
+        offsetYFun = () => frame.panel.innerHeight - 1)
+
+      stack.add(frame)
+      stack.show()
+
+    }
+  }
+
+
 }
